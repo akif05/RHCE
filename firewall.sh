6452f51f54d1d33137829bd9ce7366d42bfcd7fd
akif05 <36417283+akif05@users.noreply.github.com>
2018-10-29 19:27:06 +0000

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -6,7 +6,7 @@ firewall-cmd --permanent --change-interface=enp0s9 --zone=internal
 
 firewall-cmd --permanent --zone=external --add-masquerade
 
-firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 0 -s 192.168.2.0/25 j ACCEPT
-firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 0 -s 192.168.3.0/24 j ACCEPT
+firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 0 -s 192.168.11.0/24 j ACCEPT
+firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 0 -s 192.168.22.0/24 j ACCEPT
 
 systemctl reload firewalld
