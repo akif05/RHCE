49272621ea656533dc0a2c541a2078eaaa45f4d8
akif05 <36417283+akif05@users.noreply.github.com>
2018-10-29 22:59:26 +0000

    Update firewall_rules.sh

--- firewall_rules.sh
+++ firewall_rules.sh
@@ -1,9 +1,7 @@
 
 firewall-cmd --set-default-zone=internal
 firewall-cmd --list-services
-firewall-cmd relo
 firewall-cmd --reload
 firewall-cmd --get-active-zone
-firewall-cmd --zone=internal --add-service=ssh
 firewall-cmd --zone=internal --permanent --add-service=ssh
 firewall-cmd --reload
